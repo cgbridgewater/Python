0de01a8caf6e47183a5fda25f45faba43915e1da
cgbridgewater <cgbridgewater@outlook.com>
2022-09-26 16:36:28 -0700

    updating code lines based on lesson - strings

--- fundamentals/introduction/play_file.py
+++ fundamentals/introduction/play_file.py
@@ -65,3 +65,38 @@ print(type(int_to_complex))
 import random
 print(random.randint(2,50)) # provides a random number between 2 and 50
 
+
+print("this is a sample string")
+
+name = "Zen"
+print("My name is", name)
+
+name = "Zen"
+print("My name is " + name)
+
+total = 35
+user_val = "26"
+# total = total + user_val
+# output: TypeError
+total = total + int(user_val)
+# total will be 61
+
+# print("Hello " + 42)
+# output: TypeError
+print("Hello " + str(42))
+# output: Hello 42
+
+
+first_name = "Zen"
+last_name = "Coder"
+age = 27
+print(f"My name is {first_name} {last_name} and I am {age} years old.")
+
+first_name = "Zen"
+last_name = "Coder"
+age = 27
+print("My name is {} {} and I am {} years old.".format(first_name, last_name, age))
+# output: My name is Zen Coder and I am 27 years old.
+print("My name is {} {} and I am {} years old.".format(age, first_name, last_name))
+# output: My name is 27 Zen and I am Coder years old.
+
