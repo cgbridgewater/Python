d5e8600c9f4ac2be35bf41c73add889dcd70ef3b
cgbridgewater <cgbridgewater@outlook.com>
2022-09-28 14:47:27 -0700

    updating code per lessons loop break/continue

--- fundamentals/introduction/loops_play.py
+++ fundamentals/introduction/loops_play.py
@@ -82,13 +82,82 @@ for v in my_list:
 countries = ["Uganda", "Chile", "Albania", "Saudi Arabia"]
 
 # Challenge 1: Fix the range!
-for integer in range(0, 7):
-    print("Index:")
-    # Challenge 2: print the index here
-    print("Country:")
-    # Challenge 3: print the country here
+for integer in range(0, len(countries)):
+    print(integer)    # Challenge 2: print the index here
+    print(integer, countries[integer])   # Challenge 3: print the country here
  
+
+
+countries = ["Uganda", "Chile", "Albania", "Saudi Arabia"]
+
 # Looping over values only...
 for country in countries:
-    print("Country: ")
+    print(country)
     # Challenge 4: print the country here
+
+
+# While loops 
+for count in range(0,5):
+    print("looping =", count)
+# output 
+# looping = 0
+# looping = 1
+# looping = 2
+# looping = 3
+# looping = 4
+
+count = 0
+while count <= 5:
+    print("looping - ", count)
+    count += 1
+# Output
+# looping -  0
+# looping -  1
+# looping -  2
+# looping -  3
+# looping -  4
+# looping -  5
+
+y = 3
+while y > 0:
+    print(y)
+    y = y - 1
+else:
+    print("Final else statement")
+
+
+
+# loop break example
+for val in "string":
+    if val == "i":
+        break
+    print(val)
+# output: s, t, r
+
+for val in "string":
+    if val == "o":   # break never occurs
+        break
+    print(val)
+# output: s, t, r, i, n., g
+
+
+
+# loop continue example
+for val in "string":
+    if val == "i":
+        continue
+    print(val)
+# output: s, t, r, n, g
+# notice, no i in the output, but the loop continued after the i
+
+
+y = 3
+while y > 0:
+    print(y)
+    y = y - 1
+    if y == 0:
+        break
+else:		# only executes on a clean exit from the while loop (i.e. not a break)
+   print("Final else statement")
+# output: 3, 2, 1
+
