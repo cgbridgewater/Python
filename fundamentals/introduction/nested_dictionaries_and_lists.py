43a20394b7825664f3ef75ccdad67baf68e54123
cgbridgewater <cgbridgewater@outlook.com>
2022-09-29 19:42:04 -0700

    updated code

--- fundamentals/introduction/nested_dictionaries_and_lists.py
+++ fundamentals/introduction/nested_dictionaries_and_lists.py
@@ -1,3 +1,5 @@
+#1
+
 x = [ [5,2,3], [10,8,9] ] 
 students = [
     {'first_name':  'Michael', 'last_name' : 'Jordan'},
@@ -9,3 +11,58 @@ sports_directory = {
 }
 z = [ {'x': 10, 'y': 20} ]
 
+#  update values in dictionaries and lists
+    #1 Change the value 10 in x to 15. Once you're done, x should now be [ [5,2,3], [15,8,9] ]
+x[1][0] = 15
+print(x)
+
+    #2 Change the last_name of the first student from 'Jordan' to 'Bryant'
+students[0]['last_name'] = 'Bryant'
+print(students)
+
+    #3 In the sports_directory, change 'Messi' to 'Andres'
+sports_directory['soccer'][0] = 'Andres'
+print(sports_directory)
+
+    #4 Change the value 20 in z to 30
+z[0]['y'] = 30
+print(z)
+
+
+#2
+# iterateDictionary(students) 
+# should output: (it's okay if each key-value pair ends up on 2 separate lines;
+# bonus to get them to appear exactly as below!)
+# first_name - Michael, last_name - Jordan
+# first_name - John, last_name - Rosales
+# first_name - Mark, last_name - Guillen
+# first_name - KB, last_name - Tonel
+
+# iterateDictionary(some_list)
+
+students = [
+    {'first_name':  'Michael', 'last_name' : 'Jordan'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'},
+    {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+    {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+
+
+count = 0
+while students[] < len(students):
+    print(students[1]["last_name"])
+    count += 1
+
+
+
+
+for val, key in students[0]:
+    print(key, " = ", val)
+
+for each_key in students[0]:
+    print(students[0][each_key])
+
+for each_key in students:
+    print(students[each_key])
+
+# print(students[0]) returns   {'first_name': 'Michael', 'last_name': 'Jordan'}
