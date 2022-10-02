2018f73d9ffd4529e3e3190551cfa1c3b609946c
cgbridgewater <cgbridgewater@outlook.com>
2022-10-02 00:34:45 -0700

    updating code per lesson

--- fundamentals/oop/methods_lesson.py
+++ fundamentals/oop/methods_lesson.py
@@ -1,11 +1,21 @@
 class User:
-    def __init__(self,first_name,last_name,age):
+    def __init__(self,first_name,last_name,age, email):
         self.firt_name = first_name
         self.last_name = last_name
+        self.name = f"{first_name} {last_name}" #is this okay???
         self.age = age
+        self.email = email
 
     def greeting(self):
-        print(f"Hello, my name is {self.firt_name}!")
+        print(f"Hello, Thank you for signing up {self.firt_name}!")
 
-chris = User("Chris", "Bridgewater", 43)
+chris = User("Chris", "Bridgewater", 43, "SomeEmail@aol.com")
+chris.greeting()
+print(f"Hello, my name is {chris.name}.")
+
+adrien = User("Adrien","Dion",39,"adion@codingdojo.com")
+sadie = User("Sadie", "Flick", 35, "sflick@codingdojo.com")
+
+sadie.greeting()
+adrien.greeting()
 chris.greeting()
