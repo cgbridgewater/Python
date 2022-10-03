4cd072cd341956aa3697e6d09404ece1ac71d424
cgbridgewater <cgbridgewater@outlook.com>
2022-10-03 00:21:31 -0700

    first commit

---
+++ fundamentals/oop/class_and_static_methods_lesson.py
@@ -0,0 +1,56 @@
+class BankAccount:
+    # Declaring a class attribute
+    # Shared among all bank accounts
+    bank_name = "First National Dojo"		
+    
+    def __init__(self, int_rate, balance):
+        self.int_rate = int_rate
+        self.balance = balance
+
+
+#changing them on an instance
+adriensAccount = BankAccount()
+sadiesAccount = BankAccount()
+adriensAccount.bank_name = "Dojo Credit Union"
+    
+print(adriensAccount.bank_name)
+# output: Dojo Credit Union
+    
+print(sadiesAccount.bank_name)
+# output: First National Dojo
+
+#changing them on the entire class
+BankAccount.bank_name = "Bank of Dojo"
+    
+print(adriensAccount.bank_name)
+# output: Bank of Dojo
+    
+print(sadiesAccount.bank_name)
+# output: Bank of Dojo
+
+
+
+
+
+class BankAccount:
+    # class attributes
+    bank_name = "First National Dojo"
+    # new class attribute - a list of all the accounts!
+    all_accounts = []
+    
+    def __init__(self, int_rate,balance):
+        self.int_rate = int_rate
+        self.balance = balance
+        BankAccount.all_accounts.append(self)
+    # class method to change the name of the bank
+    @classmethod
+    def change_bank_name(cls,name):
+        cls.bank_name = name
+    # class method to get balance of all accounts
+    @classmethod
+    def all_balances(cls):
+        sum = 0
+        # we use cls to refer to the class
+        for account in cls.all_accounts:
+            sum += account.balance
+        return sum
