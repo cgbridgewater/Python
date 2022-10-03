9cc51fde5b595617e0e4a7498b6b9d87bc589c9d
cgbridgewater <cgbridgewater@outlook.com>
2022-10-03 00:29:51 -0700

    updated per lessons

--- fundamentals/oop/class_and_static_methods_lesson.py
+++ fundamentals/oop/class_and_static_methods_lesson.py
@@ -1,32 +1,23 @@
-class BankAccount:
-    # Declaring a class attribute
-    # Shared among all bank accounts
-    bank_name = "First National Dojo"		
-    
-    def __init__(self, int_rate, balance):
-        self.int_rate = int_rate
-        self.balance = balance
 
+# #changing them on an instance
+# adriensAccount = BankAccount()
+# sadiesAccount = BankAccount()
+# adriensAccount.bank_name = "Dojo Credit Union"
+    
+# print(adriensAccount.bank_name)
+# # output: Dojo Credit Union
+    
+# print(sadiesAccount.bank_name)
+# # output: First National Dojo
 
-#changing them on an instance
-adriensAccount = BankAccount()
-sadiesAccount = BankAccount()
-adriensAccount.bank_name = "Dojo Credit Union"
+# #changing them on the entire class
+# BankAccount.bank_name = "Bank of Dojo"
     
-print(adriensAccount.bank_name)
-# output: Dojo Credit Union
+# print(adriensAccount.bank_name)
+# # output: Bank of Dojo
     
-print(sadiesAccount.bank_name)
-# output: First National Dojo
-
-#changing them on the entire class
-BankAccount.bank_name = "Bank of Dojo"
-    
-print(adriensAccount.bank_name)
-# output: Bank of Dojo
-    
-print(sadiesAccount.bank_name)
-# output: Bank of Dojo
+# print(sadiesAccount.bank_name)
+# # output: Bank of Dojo
 
 
 
