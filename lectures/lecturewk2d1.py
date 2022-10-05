10c2fe64cdda4804c77ee7a7488b12a3e4a8172d
cgbridgewater <cgbridgewater@outlook.com>
2022-10-04 17:42:12 -0700

    first commit

--- lectures/lecturewk2d1.py
+++ lectures/lecturewk2d1.py
@@ -1,1 +1,27 @@
- 
+### 'for loop' examples
+
+## print 0-9
+for i in range(10):
+    print(i)
+
+
+## print 3-9
+for num in range(3,10):
+    print(num)
+
+
+## print 3-9 increments of 3
+for nums in range(3,10,3):
+    print(nums)
+
+
+_list = [{"name":"nathan","age":31}, {"name":"billybob", "age":33}]
+## print index + variable
+for index in _list:
+    print(index)
+
+
+## print at index + variable
+print(_list[0])
+## print first index and key 'name'
+print(_list[0]["name"])
