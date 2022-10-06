76679e119923b19d67f6ec8b6928dbbf72e4fcda
cgbridgewater <cgbridgewater@outlook.com>
2022-10-05 22:47:16 -0700

    first commit

---
+++ fundamentals/oop/inputs_and_outputs.py
@@ -0,0 +1,5 @@
+favorite_color = input('What is your favorite color? ') # input takes a prompt, which needs to be a string
+print(f'Your favorite color is: {favorite_color}') #output, prints the color given to the console
+
+dog_name = input("What is your dogs name?")
+print(f"Your dog's name is {dog_name}")
