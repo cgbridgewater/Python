d873bfe8ef3283bc645f2cbae36056eb48758918
cgbridgewater <cgbridgewater@outlook.com>
2022-10-05 22:18:28 -0700

    updating code

--- fundamentals/oop/assignment_dojo_pets.py
+++ fundamentals/oop/assignment_dojo_pets.py
@@ -1,45 +1,41 @@
-
 class Ninja:
     def __init__(self, first_name, last_name, treats):
         self.first_name = first_name
         self.last_name = last_name
         self.treats = treats
-        # self.pet = cat
 
 
     def walk(self):
         print(f"Going out for a walk")
     
 
-    def feed(self, treats):
-        print(f"Eating some {treats}")
+    def feed(self):
+        print(f"Eating some snacks")
 
-    def bathe(self, pet):    
-        print(f"squeaky clean now!")
+    def bathe(self):    
+        print(f"Squeaky clean now!")
 
-
-
-    # implement the following methods:
-    # walk() - walks the ninja's pet invoking the pet play() method
-    # feed() - feeds the ninja's pet invoking the pet eat() method
-    # bathe() - cleans the ninja's pet invoking the pet noise() method
 
 
 
 
 class Pet(Ninja): 
-    def __init__(self, name, type, tricks, noise, pet_food):
+    def __init__(self, name, type, tricks, noise, pet_food, action):
         self.name = name
         self.type = type
         self.tricks = tricks
-        self.noise = noise
+        self._noise = noise
         self.pet_food = pet_food
+        self.action = action
         self.energy = 70
         self.health = 70
 
     def add_energy(self,amount):
         if self.energy == 100:
             print("Already at full energy")
+        elif self.energy + amount >= 100:
+            self.energy = 100
+            print(f"health is now - {self.energy}")
         else:
             self.energy += amount
             print(f"energy is now - {self.energy}")
@@ -48,6 +44,9 @@ class Pet(Ninja):
     def add_health(self,amount):
         if self.health == 100:
             print("Already at full health")
+        elif self.health + amount >= 100:
+            self.health = 100
+            print(f"health is now - {self.health}")
         else:
             self.health += amount
             print(f"health is now - {self.health}")
@@ -61,7 +60,7 @@ class Pet(Ninja):
 
 
     def eat(self):
-        super().feed(self.pet_food)
+        super().feed()
         # self.noise()
         self.add_energy(5)
         self.add_health(10)
@@ -73,38 +72,57 @@ class Pet(Ninja):
         print(f"{self.name} says 'Play Time'")
 
     def noise(self):
-        print(f"{self.name} makes noises of joy {self.noise}")
+        print(f"{self.name} makes noises of joy {self._noise}")
 
-    # implement the following methods:
-    # sleep() - increases the pets energy by 25
-    # eat() - increases the pet's energy by 5 & health by 10
-    # play() - increases the pet's health by 5
-    # noise() - prints out the pet's sound
+    def say_no_food(self):
+        print(f"After telling {self.name} he can't have any food because hes chonky. {self.name} gets mad and {self.action} you")
 
 
 
-chris = Ninja("chris","B.","snacks")
-cat = Pet("Wiskers", "cat", "jumps", "purrs", "kitty food")
+
+chris = Ninja("chris","B.","tacos")
+cat = Pet("Wiskers", "cat", "jumps", "purrs", "kitty food","scratches")
+dog = Pet("Sparky","dog","runs in circles", "barks","kibble", "bites")
 
 
-# chris.pet = cat
-# chris.feed()
-
-print('''----cat eat---''')
-cat.eat()
-
-
-print('''
-
-
----chris walk---''')
+print("check feed")
+chris.feed()
+print("check bathe")
+chris.bathe()
+print("check walk")
 chris.walk()
 
+print('''
+
+
+''')
+
+
+print('''----cat does eat---''')
+cat.eat()
+print('''----cat does play--''')
+cat.play()
+print('''----cat does sleep---''')
+cat.sleep()
+print('''----cat does bath---''')
+cat.bathe()
+print('''----cat does noise ---''')
+cat.noise()
+print('''----cat does action---''')
+cat.say_no_food()
+
 
 print('''
 
 
----cat play---''')
-cat.play()
+''')
 
-# cat.eat()
+
+
+print('''----dog does things now---''')
+dog.eat()
+dog.play()
+dog.sleep()
+dog.bathe()
+dog.noise()
+dog.say_no_food()
