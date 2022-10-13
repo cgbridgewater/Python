6b4a77045d5888f245009e4b84d3c2dd1b6a0b97
cgbridgewater <cgbridgewater@outlook.com>
2022-10-12 17:11:18 -0700

    update lines

--- flask/fundamentals/hello_flask/server.py
+++ flask/fundamentals/hello_flask/server.py
@@ -16,11 +16,20 @@ def hello(banana):
 def hola(banana, num):
     return f"Hola {banana * num}"
 
-@app.route('/users/<username>/<id>')
-def show_user_profile(username,id):
-    print(username)
-    print(id)
-    return "username: " + username + ", id: " + id
+# @app.route('/users/<username>/<id>')
+# def show_user_profile(username,id):
+#     print(username)
+#     print(id)
+#     return "username: " + username + ", id: " + id
+@app.route('/repeat/<int:num>/<string:word>')
+def repeat_word(num, word):
+    output = ''
+
+    for i in range(0,num):
+        output += f"<p>{word}</p>"
+
+    return output
+
 
 if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
     app.run(debug=True)    # Run the app in debug mode.
