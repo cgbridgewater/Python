fa3910e9e3b8256371811838a2abcfd94ff0e38d
cgbridgewater <cgbridgewater@outlook.com>
2022-10-13 12:05:57 -0700

    updates with lessons

--- flask/fundamentals/lesson_wk3d2/server.py
+++ flask/fundamentals/lesson_wk3d2/server.py
@@ -1,8 +1,26 @@
-from flask import Flask  # Import Flask to allow us to create our app
-app = Flask(__name__)    # Create a new instance of the Flask class called "app"
-@app.route('/')          # The "@" decorator associates this route with the function immediately following
-def hello_world():
-    return 'Hello World!'  # Return the string 'Hello World!' as a response
-if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
-    app.run(debug=True)    # Run the app in debug mode.
+from flask import Flask
 
+app = Flask(__name__)
+
+
+
+@app.route('/')         
+def root():
+    return 'Root Route'
+
+
+@app.route('/home')
+def dashboard():
+    return "Houston we have a problem"
+
+
+
+@app.route('/', defaults = {'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    return "nope, try again"
+
+
+
+if __name__=="__main__":
+    app.run(debug=True, port = 5000)
