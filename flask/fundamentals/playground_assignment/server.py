21a536ee64b7feae972fd75a7012c6262b2d192f
cgbridgewater <cgbridgewater@outlook.com>
2022-10-14 15:23:14 -0700

    finalized and tested

--- flask/fundamentals/playground_assignment/server.py
+++ flask/fundamentals/playground_assignment/server.py
@@ -1,26 +1,37 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
-#HOME PAGE
+###HOME PAGE
 @app.route('/')          
 def hello_world():
     return render_template('index.html')
 
-#PLAY PAGE WITH STACKED INPUT OPTIONS
+###PLAY PAGE ROUTES WITH STACKED INPUT OPTIONS
 @app.route('/play/', endpoint='play')
 @app.route('/play/<int:num>/', endpoint='<int:num>')
 @app.route('/play/<int:num>/<string:color>/', endpoint='<int:num>/<string:color>')
-def play(num=None,color=None): 
+def play(num= 3,color=None): 
+
+### LOOP PRODUCES DIVS THAT THE BOXES ARE CREATED FROM
+    word= ''
+    output = ''
+    for i in range(0,num):
+        output += f" {word} "
+        # list.append(output)
+
+### IF STATEMENTS TO RETURN THE ROUTE REPLY
     if request.endpoint == '/play':
-        return render_template("play.html")    
+        return render_template("play.html")   
+    
     if request.endpoint == '<int:num>':
-        return render_template("play.html", num=3 )
+        return render_template("play.html", num = num )
+    
     if request.endpoint == '<int:num>/<string:color>':
-        return render_template("play.html", num=3, color = color )    
+        return render_template("play.html", num = num, color = color )    
+    
     else:
         return render_template("play.html")
 
 
 if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
     app.run(debug=True)    # Run the app in debug mode.
-
