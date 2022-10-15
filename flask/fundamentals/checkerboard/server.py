7d6132b18334072f3e3c6117da3a6d003806ff3f
cgbridgewater <cgbridgewater@outlook.com>
2022-10-14 21:42:14 -0700

    finalized and tested code per assignment

--- flask/fundamentals/checkerboard/server.py
+++ flask/fundamentals/checkerboard/server.py
@@ -2,64 +2,29 @@ from flask import Flask, render_template, request  # Import Flask to allow us to
 app = Flask(__name__)    # Create a new instance of the Flask class called "app"
 
 
-
-
-### MAIN PAGE ROUTE
-# @app.route('/')
-# def home_page():
-#     return render_template("index.html")
-
-
-# @app.route('/4')
-# def eightby4():
-#     return render_template("index_4.html")
-
-
-
-
-###PLAY PAGE ROUTES WITH STACKED INPUT OPTIONS
+### INDEX ROUTES WITH STACKED INPUT OPTIONS - MAKES CHECKERS PAGE
 @app.route('/')
 @app.route('/<int:num>/', endpoint='<int:num>')
-@app.route('/<int:num>/<int:nums>/', endpoint='/<int:num>/<int:nums>/')
-def play(num= 8, nums = 8): 
+@app.route('/<int:num>/<int:nums>/', endpoint='/<int:nums>/')
+@app.route('/<int:num>/<int:nums>/<string:color>/', endpoint='<string:color>/')
+@app.route('/<int:num>/<int:nums>/<string:color>/<string:other_color>/', endpoint='<string:other_color>/')
+def play(num= 8, nums = 8, color = None, other_color = None): 
 
-### LOOP PRODUCES DIVS THAT THE BOXES ARE CREATED FROM
-    # even= ''
-    # odd = ''
-    # output1 = ''
-    # output2 = ''
-    # for i in range(0,num):
-    #     if i %2 == 0:
-    #         output1 += f" {even} "
-    #     else:
-    #         output2 += f" {odd} "
 
-    height = ' '
-    output1 = ''
-    for i in range(0,num):
-        output1 += f" {height} "
-
-    width = ' '
-    output2 = ''
-    for x in range(0,nums):
-        output2 += f" {width} "
-
-### IF STATEMENTS TO RETURN THE ROUTE REPLY
-    # if request.endpoint == '/4':
-    #     return render_template("index_4.html")
-    
     if request.endpoint == '<int:num>':
-        return render_template("index_4.html", num = num )
+        return render_template("index.html", num = num )
     
-    if request.endpoint == '/<int:num>/<int:nums>/':
-        return render_template("index_4.html", num = num, nums = nums )    
+    if request.endpoint == '/<int:nums>/':
+        return render_template("index.html", num = num, nums = nums )    
+    
+    if request.endpoint == '<string:color>/':
+        return render_template("index.html", num = num, nums = nums, color = color )    
+    
+    if request.endpoint == '<string:other_color>/':
+        return render_template("index.html", num = num, nums = nums,color = color, other_color = other_color )    
     
     else:
-        return render_template("index_4.html")
-
-
-
-
+        return render_template("index.html",  num = num )
 
 
 ### DINO GAME
@@ -69,6 +34,8 @@ def catch_all(path):
     return render_template("dinosaur.html")
 
 
-if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
-    app.run(debug=True)    # Run the app in debug mode.
+### Ensure this file is being run directly and not from a different module
+### Run the app in debug mode.
+if __name__=="__main__":
+    app.run(debug=True)
 
