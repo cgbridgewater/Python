9c249a4b172ab716e33117bd74ab8c709b8c74aa
cgbridgewater <cgbridgewater@outlook.com>
2022-10-17 16:05:57 -0700

    updates

--- flask/fundamentals/hello_flask/server.py
+++ flask/fundamentals/hello_flask/server.py
@@ -10,10 +10,6 @@ def success():
     return "Great Success!!" #returns string as a response
 
 
-@app.route('/lists')
-def renderList():
-    return render_template('list.html', students = studentInfo, list=)
-
 #### this one is the working command!!!!! ####
 
 @app.route('/hello/<string:banana>/<int:num>')
@@ -57,6 +53,24 @@ def repeat_word(num, word):
         output += f"<p>{word}</p>"
 
     return output
+
+
+@app.route('/lists')
+def render_lists():
+    # Soon enough, we'll get data from a database, but for now, we're hard coding data
+    student_info = [
+        {'name' : 'Michael', 'age' : 35},
+        {'name' : 'John', 'age' : 30 },
+        {'name' : 'Mark', 'age' : 25},
+        {'name' : 'KB', 'age' : 27}
+    ]
+    return render_template("lists.html", random_numbers = [3,1,5], students = student_info)
+
+
+
+
+
+
 
 
 if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
