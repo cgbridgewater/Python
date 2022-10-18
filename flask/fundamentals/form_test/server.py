101bb1aec9ad9101dde411323873265ade5660b8
cgbridgewater <cgbridgewater@outlook.com>
2022-10-18 12:59:14 -0700

    first commit

---
+++ flask/fundamentals/form_test/server.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+app = Flask(__name__)  
+
+
+
+from flask import Flask, render_template, request, redirect # added request
+            
+@app.route('/users', methods=['POST'])
+def create_user():
+    print("Got Post Info")
+    print(request.form)
+    # Never render a template on a POST request.
+    # Instead we will redirect to our index route.
+    return redirect('/')
+
+
+
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+
+
+
+if __name__=="__main__":  
+    app.run(debug=True)   
+
