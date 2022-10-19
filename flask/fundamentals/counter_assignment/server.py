601b3c659f93e998f8975970199962bcf368e02c
cgbridgewater <cgbridgewater@outlook.com>
2022-10-19 13:21:36 -0700

    update code for adding number form

--- flask/fundamentals/counter_assignment/server.py
+++ flask/fundamentals/counter_assignment/server.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)   
 app.secret_key = 'mums the word'
 
-### DO I EVEN NEED THIS??  DEFAULT PAGE
+### DO I EVEN NEED THIS??  FORWARDS TO DEFAULT HOME PAGE
 @app.route('/')
 def index():
     return redirect("/home")
@@ -10,48 +10,46 @@ def index():
 ### HOME PAGE - THIS IS WHERE THE MAGIC HAPPENS
 @app.route('/home', methods = ['GET'])
 def home():
-    print('WELCOME HOME!')
 ### IF STATEMENT FOR COUNTER IN SESSION
     if not 'counter' in session:
         session['counter'] =0        
         session['hits'] =0
-        print('counter isnt moving')
     else:
         session['counter'] += 1
-        print('counters going')
 ### IF STATEMENT FOR HITS IN SESSION
     if not 'hits' in session:
         session['hits'] =0
-        print('counter isnt moving')
     else:
         session['hits'] += 1
-        print('counters going')
     return render_template("index.html")
 
 ### SINGLE UP COUNTER
 @app.route('/plusone')
 def plus_one():
-    print('ONE ADDED TO THE COUNTER!!! ')
     return redirect("/home")
 
 ### ADDS TWO HITS ONTO THE SITE HITS
 @app.route('/plustwo')
 def plus_two():
-    print('TWO ADDED TO THE COUNTER!!! ')
     session['counter'] +=1
+    return redirect("/home")
+
+### ADD ON - ADDS ON 'X' NUMBER TO THE SESSION COUNTER
+@app.route('/addon', methods = ['POST'])
+def add_on():
+    session['nums'] = request.form['nums']
+    session['counter'] += int(session['nums'])-1
     return redirect("/home")
 
 ### RESETS SITE VISITS COUNTER
 @app.route('/clear')
 def clear_session():
-    print('COUNTER CLEARED!!!')
     session['counter'] =0
     return redirect("/home")
 
 ### ENDS SESSION AND RESETS COUNTERS TO ZERO
 @app.route('/destroy')
 def destroy_session():
-    print('SESSION ENDED!!!')
     session.clear()
     session['counter'] =0
     session['hits'] =0
@@ -66,4 +64,3 @@ def catch_all(path):
 ### DEBUGGING
 if __name__=="__main__":   
     app.run(debug=True) 
-
