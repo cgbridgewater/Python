84c2fe2f7851abe05bb5989e738225576fe0bb3c
cgbridgewater <cgbridgewater@outlook.com>
2022-10-19 00:03:32 -0700

    edited

--- flask/fundamentals/dojo_fruit_store/server.py
+++ flask/fundamentals/dojo_fruit_store/server.py
@@ -1,14 +1,38 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect,session
 app = Flask(__name__)  
+app.secret_key = 'keep it secret, keep it safe' # set a secret key for security purposes
 
-@app.route('/')         
+
+
+@app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route('/checkout', methods=['POST'])         
+
+
+@app.route('/users', methods = ['POST'])         
+def create_user():
+    print("Got Post Info")
+    session['first_name'] = request.form['first_name']
+    session['last_name'] = request.form['last_name']
+    session['email'] = request.form ['email']
+    return redirect("/checkout")
+
+
+
+
+@app.route('/checkout')         
 def checkout():
-    print(request.form)
+
     return render_template("checkout.html")
+    #  , last_name = lastName, email = student_id)
+
+
+
+
+
+
+
 
 @app.route('/fruits')         
 def fruits():
--- flask/fundamentals/form_test/server.py
+++ flask/fundamentals/form_test/server.py
@@ -1,20 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request,redirect,session
 app = Flask(__name__)  
+app.secret_key = 'keep it secret, keep it safe' # set a secret key for security purposes
 
 
 
-from flask import Flask, render_template, request, redirect # added request
-            
+
 @app.route('/users', methods=['POST'])
 def create_user():
     print("Got Post Info")
-    print(request.form)
-    # Never render a template on a POST request.
-    # Instead we will redirect to our index route.
-    return redirect('/')
 
+    session['username'] = request.form['name']
+    session['useremail'] = request.form ['email']
+    return redirect('/show')
 
-
+@app.route("/show")
+def show_user():
+    # print("Showing the User Info From the Form")
+    # print(request.form)
+    return render_template("show.html", name_on_template=session['username'], email_on_template= session['useremail'])
 
 
 @app.route('/')
