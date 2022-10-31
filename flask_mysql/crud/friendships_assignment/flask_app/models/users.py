871b0a22c66478994ee24dccaf455b9ae0aaed4e
cgbridgewater <cgbridgewater@outlook.com>
2022-10-30 20:51:32 -0700

    code updates

--- flask_mysql/crud/friendships_assignment/flask_app/models/users.py
+++ flask_mysql/crud/friendships_assignment/flask_app/models/users.py
@@ -1,9 +1,5 @@
 from flask_app.config.mysqlconnection import connectToMySQL
-
-#### CRUD EXAMPLES BELOW!!!! 
-
-
-
+from pprint import pprint
 
 
 ### USER CLASS
@@ -11,51 +7,75 @@ class User:
     def __init__(self,data):
         self.id = data['id']
         self.first_name = data['first_name']
-        self.last_name = data['last_name']
-        self.email = data['email']    
+        self.last_name = data['last_name'] 
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
-
-
+        self.friendships = []
 
 
 ### GET ALL USERS (WORKING)
     @classmethod
     def get_all(cls):
         query = "SELECT * FROM users;"
-        results = connectToMySQL('users_cr').query_db(query)
-        users = []
-        for i in results:
-            users.append(cls(i))
-        return users
-
-
+        return connectToMySQL('friendship_schema').query_db(query)
 
 
 ### CREATE AND SAVE NEW USER (WORKING)
     @classmethod
-    def save(cls,data):
-        query = "INSERT INTO users (first_name, last_name,email) VALUES (%(first_name)s, %(last_name)s, %(email)s);"
-        return connectToMySQL('users_cr').query_db(query,data)
+    def saveUser(cls,data):
+        query = "INSERT INTO users (first_name, last_name) VALUES (%(first_name)s, %(last_name)s);"
+        return connectToMySQL('friendship_schema').query_db(query,data)
+
+
+### CREATE AND SAVE NEW FRIENDSHIP (WORKING)
+    @classmethod
+    def saveFriendship(cls,data):
+        query = "INSERT INTO friendships (user_id, friend_id) VALUES (%(user_id)s, %(friend_id)s);"
+        return connectToMySQL('friendship_schema').query_db(query,data)
 
 
 
-### GET USER BY ID (WORKING)
+
+### GET USER AND FRIENDS (testing)
+    @classmethod
+    def getFriends(cls):
+        query = "SELECT f.first_name as friend_first , f.last_name as friend_last , u.first_name as user_first , u.last_name as user_last FROM users u LEFT JOIN friendships fs ON u.id = fs.user_id LEFT JOIN users f ON f.id = fs.friend_id;"
+        result = connectToMySQL('friendship_schema').query_db(query) 
+        pprint(result)
+        return result 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+### GET USER BY ID (testing)
     @classmethod
     def get_one_user(cls,data):
         query = "SELECT * FROM users WHERE id = %(id)s;"
-        result = connectToMySQL('users_cr').query_db(query,data)
+        result = connectToMySQL('friendship_schema').query_db(query,data)
         if len(result) == 0: #if no users found, return an empty list
             return None
         else: # if at least one user found
             return cls(result[0])
 
 
-### UPDATE USER BY ID (WORKING)
+### UPDATE USER BY ID (testing)
     @classmethod
     def updateUser(cls,data):
-        query = "UPDATE users SET first_name = %(first_name)s, last_name = %(last_name)s, email = %(email)s WHERE id = %(id)s;"
-        return connectToMySQL('users_cr').query_db(query,data)
+        query = "UPDATE users SET first_name = %(first_name)s, last_name = %(last_name)s WHERE id = %(id)s;"
+        return connectToMySQL('friendship_schema').query_db(query,data)
 
 
 
@@ -63,4 +83,4 @@ class User:
     @classmethod
     def deleteUser(cls,data):
         query = "DELETE FROM users WHERE id = %(id)s;"
-        return connectToMySQL('users_cr').query_db(query,data) 
+        return connectToMySQL('friendship_schema').query_db(query,data) 
